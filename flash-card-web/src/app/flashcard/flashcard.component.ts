837343501003864da96204387beb72bdd6013ea4
Meet Patel <meetpatel3299@icloud.com>
2023-04-22 15:46:02 -0500

    Progress in cards contruction

---
+++ flash-card-web/src/app/flashcard/flashcard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flashcard',
+  templateUrl: './flashcard.component.html',
+  styleUrls: ['./flashcard.component.css']
+})
+export class FlashcardComponent {
+
+}
